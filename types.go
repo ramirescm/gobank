7cae200f787d63502b1f25df4d03044ddb874460
Ramires <ramirescm@gmail.com>
2023-09-19 00:02:53 -0300

    added types

--- types.go
+++ types.go
@@ -3,7 +3,19 @@ package main
 import (
 	"math/rand"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+type LoginReponse struct {
+	Number int64  `json:"number"`
+	Token  string `json:"token"`
+}
+
+type LoginRequest struct {
+	Number   int64  `json:"number"`
+	Password string `json:"password"`
+}
 
 type TransferRequest struct {
 	ToAccount  int `json:"toAccount"`
@@ -14,24 +26,36 @@ type TransferRequest struct {
 type CreateAccountRequest struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
+	Password  string `json:"password"`
 }
 
 type Account struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Number    int64     `json:"number"`
-	Balance   int64     `json:"balance"`
-	CreatedAt time.Time `json:"createdAt"`
+	ID                int       `json:"id"`
+	FirstName         string    `json:"firstName"`
+	LastName          string    `json:"lastName"`
+	EncryptedPassword string    `json:"-"`
+	Number            int64     `json:"number"`
+	Balance           int64     `json:"balance"`
+	CreatedAt         time.Time `json:"createdAt"`
 }
 
-func NewAccount(firstName, lastName string) *Account {
+func (a *Account) ValidPassword(pw string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(a.EncryptedPassword), []byte(pw)) == nil
+}
+
+func NewAccount(firstName, lastName, password string) (*Account, error) {
+	encrypt, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Account{
 		// ID:        int(rand.Intn(100000)),
-		FirstName: firstName,
-		LastName:  lastName,
-		Number:    int64(rand.Intn(20000)),
-		Balance:   int64(rand.Intn(20000)),
-		CreatedAt: time.Now().UTC(),
-	}
+		FirstName:         firstName,
+		LastName:          lastName,
+		EncryptedPassword: string(encrypt),
+		Number:            int64(rand.Intn(20000)),
+		Balance:           int64(rand.Intn(20000)),
+		CreatedAt:         time.Now().UTC(),
+	}, nil
 }
